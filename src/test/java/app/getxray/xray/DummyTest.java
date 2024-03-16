706b12efaaa8703b03783b599a005daad73a699e
Sergio Freire <sergio.freire@gmail.com>
2024-03-16 16:14:15 +0000

    additional unit tests

--- src/test/java/app/getxray/xray/DummyTest.java
+++ src/test/java/app/getxray/xray/DummyTest.java
@@ -11,12 +11,50 @@ class DummyTest {
     
     @Test
     void resultsImporterCloudBuilderTest() {
-        CloudBuilder xrayImporterBuilder = new XrayResultsImporter.CloudBuilder("clientId", "clientSecret");
+        CloudBuilder xrayImporterBuilder = new XrayResultsImporter.CloudBuilder("clientId", "clientSecret")
+            .withProjectKey("CALC")
+            .withVersion("1.0")
+            .withRevision("1234")
+            .withTestPlanKey("CALC-124")
+            .withTestExecKey("CALC-200")
+            .withTestEnvironment("chrome")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true);
         assertNotNull(xrayImporterBuilder.build());
     }
 
     void resultsImporterDCBuilderTest() {
-        ServerDCBuilder xrayImporterBuilder = new XrayResultsImporter.ServerDCBuilder("https://jira.example.com", "000000");
+        ServerDCBuilder xrayImporterBuilder = new XrayResultsImporter.ServerDCBuilder("https://jira.example.com", "000000")
+            .withProjectKey("CALC")
+            .withVersion("1.0")
+            .withRevision("1234")
+            .withTestPlanKey("CALC-124")
+            .withTestExecKey("CALC-200")
+            .withTestEnvironment("chrome")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true);
         assertNotNull(xrayImporterBuilder.build());
+    }
+
+    @Test
+    void exportFeaturesCloudBuilderTest() {
+        XrayFeaturesExporter.CloudBuilder xrayBuilder = new XrayFeaturesExporter.CloudBuilder("clientId", "clientSecret");
+        assertNotNull(xrayBuilder.build());
+    }
+
+    void exportFeaturesDCBuilderTest() {
+        XrayFeaturesExporter.ServerDCBuilder xrayBuilder = new XrayFeaturesExporter.ServerDCBuilder("https://jira.example.com", "000000");
+        assertNotNull(xrayBuilder.build());
+    }
+
+    @Test
+    void importFeaturesCloudBuilderTest() {
+        XrayFeaturesImporter.CloudBuilder xrayBuilder = new XrayFeaturesImporter.CloudBuilder("clientId", "clientSecret");
+        assertNotNull(xrayBuilder.build());
+    }
+
+    void importFeaturesDCBuilderTest() {
+        XrayFeaturesImporter.ServerDCBuilder xrayBuilder = new XrayFeaturesImporter.ServerDCBuilder("https://jira.example.com", "000000");
+        assertNotNull(xrayBuilder.build());
     }
 }
