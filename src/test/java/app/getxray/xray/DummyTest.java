ee193d3fba9e460e693828fd5a404f792689e677
Sergio Freire <sergio.freire@gmail.com>
2024-03-17 11:23:54 +0000

    additional unit and integration tests

--- src/test/java/app/getxray/xray/DummyTest.java
+++ src/test/java/app/getxray/xray/DummyTest.java
@@ -1,6 +1,11 @@
 package app.getxray.xray;
 
+import static app.getxray.xray.CommonUtils.isTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,11 +24,13 @@ class DummyTest {
             .withTestExecKey("CALC-200")
             .withTestEnvironment("chrome")
             .withIgnoreSslErrors(true)
-            .withVerbose(true);
+            .withVerbose(true)
+            .withTimeout(5);
         assertNotNull(xrayImporterBuilder.build());
     }
 
-    void resultsImporterDCBuilderTest() {
+    @Test
+    void resultsImporterDCBuilderWithPersonalAccessTokenTest() {
         ServerDCBuilder xrayImporterBuilder = new XrayResultsImporter.ServerDCBuilder("https://jira.example.com", "000000")
             .withProjectKey("CALC")
             .withVersion("1.0")
@@ -32,29 +39,106 @@ class DummyTest {
             .withTestExecKey("CALC-200")
             .withTestEnvironment("chrome")
             .withIgnoreSslErrors(true)
-            .withVerbose(true);
+            .withVerbose(true)
+            .withTimeout(5);
+        assertNotNull(xrayImporterBuilder.build());
+    }
+
+    @Test
+    void resultsImporterDCBuilderWithUsernameAndPasswordTest() {
+        ServerDCBuilder xrayImporterBuilder = new XrayResultsImporter.ServerDCBuilder("https://jira.example.com", "jiraUsername", "jiraPassword")
+            .withProjectKey("CALC")
+            .withVersion("1.0")
+            .withRevision("1234")
+            .withTestPlanKey("CALC-124")
+            .withTestExecKey("CALC-200")
+            .withTestEnvironment("chrome")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true)
+            .withTimeout(5);
         assertNotNull(xrayImporterBuilder.build());
     }
 
     @Test
     void exportFeaturesCloudBuilderTest() {
-        XrayFeaturesExporter.CloudBuilder xrayBuilder = new XrayFeaturesExporter.CloudBuilder("clientId", "clientSecret");
+        XrayFeaturesExporter.CloudBuilder xrayBuilder = new XrayFeaturesExporter.CloudBuilder("clientId", "clientSecret")
+            .withIssueKeys("CALC-1")
+            .withFilterId("1234")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true)
+            .withTimeout(5);
         assertNotNull(xrayBuilder.build());
     }
 
-    void exportFeaturesDCBuilderTest() {
-        XrayFeaturesExporter.ServerDCBuilder xrayBuilder = new XrayFeaturesExporter.ServerDCBuilder("https://jira.example.com", "000000");
+    @Test
+    void exportFeaturesDCBuilderWithPersonalAccessTokenTest() {
+        XrayFeaturesExporter.ServerDCBuilder xrayBuilder = new XrayFeaturesExporter.ServerDCBuilder("https://jira.example.com", "000000")
+            .withIssueKeys("CALC-1")
+            .withFilterId("1234")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true)
+            .withTimeout(5);
+        assertNotNull(xrayBuilder.build());
+    }
+
+    @Test
+    void exportFeaturesDCBuilderWithUsernameAndPasswordTest() {
+        XrayFeaturesExporter.ServerDCBuilder xrayBuilder = new XrayFeaturesExporter.ServerDCBuilder("https://jira.example.com", "jiraUsername", "jiraPassword")
+            .withIssueKeys("CALC-1")
+            .withFilterId("1234")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true)
+            .withTimeout(5);
         assertNotNull(xrayBuilder.build());
     }
 
     @Test
     void importFeaturesCloudBuilderTest() {
-        XrayFeaturesImporter.CloudBuilder xrayBuilder = new XrayFeaturesImporter.CloudBuilder("clientId", "clientSecret");
+        XrayFeaturesImporter.CloudBuilder xrayBuilder = new XrayFeaturesImporter.CloudBuilder("clientId", "clientSecret")
+            .withProjectKey("CALC")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true)
+            .withTimeout(5);
         assertNotNull(xrayBuilder.build());
     }
 
+    @Test
     void importFeaturesDCBuilderTest() {
-        XrayFeaturesImporter.ServerDCBuilder xrayBuilder = new XrayFeaturesImporter.ServerDCBuilder("https://jira.example.com", "000000");
+        XrayFeaturesImporter.ServerDCBuilder xrayBuilder = new XrayFeaturesImporter.ServerDCBuilder("https://jira.example.com", "jiraUsername", "jiraPassword")
+            .withProjectKey("CALC")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true)
+            .withTimeout(5);
         assertNotNull(xrayBuilder.build());
     }
+
+    @Test
+    void importFeaturesDCBuilderWithPersonalAccessTokenTest() {
+        XrayFeaturesImporter.ServerDCBuilder xrayBuilder = new XrayFeaturesImporter.ServerDCBuilder("https://jira.example.com", "000000")
+            .withProjectKey("CALC")
+            .withIgnoreSslErrors(true)
+            .withVerbose(true)
+            .withTimeout(5);
+        assertNotNull(xrayBuilder.build());
+    }
+
+    @Test
+    void isTrueTruethnessTest() {
+        assertAll("conditions for truethness", 
+            () -> assertTrue(isTrue(Boolean.TRUE)),
+            () -> assertTrue(isTrue(true))
+        );
+    }
+
+    @Test
+    void isTrueFalseTest() {
+        assertAll("conditions for false", 
+            () -> assertFalse(isTrue(Boolean.FALSE)),
+            () -> assertFalse(isTrue(false)),
+            () -> assertFalse(isTrue(null))
+        );
+    }
+
+
+
 }
--- src/test/java/app/getxray/xray/it/import_results/XrayDatacenterIT.java
+++ src/test/java/app/getxray/xray/it/import_results/XrayDatacenterIT.java
@@ -229,6 +229,41 @@ public class XrayDatacenterIT {
  @MavenGoal("xray:import-results")
  @SystemProperty(value = "xray.cloud", content = "false")
  @SystemProperty(value = "xray.jiraBaseUrl", content = "http://127.0.0.1:"+PORT_NUMBER)
+ @SystemProperty(value = "xray.jiraToken", content = "00112233445566778899")
+ @SystemProperty(value = "xray.reportFormat", content = "junit")
+ @SystemProperty(value = "xray.reportFile", content = "junit.xml")
+ @SystemProperty(value = "xray.projectKey", content = "CALC")
+ @SystemProperty(value = "xray.testExecKey", content = "CALC-2")
+ @SystemProperty(value = "xray.testPlanKey", content = "CALC-3")
+ @SystemProperty(value = "xray.version", content = "1.0")
+ @SystemProperty(value = "xray.revision", content = "123")
+ @SystemProperty(value = "xray.testEnvironment", content = "chrome")
+ void junit_standard_using_personal_access_token(MavenExecutionResult result) throws IOException {
+    String report = CommonUtils.readResourceFileForImportResults("XrayDatacenterIT/junit_standard_using_personal_access_token/junit.xml");
+
+    wm.verify(
+        postRequestedFor(urlPathEqualTo("/rest/raven/2.0/import/execution/junit"))
+            .withHeader("Authorization", equalTo("Bearer 00112233445566778899"))
+            .withHeader("Content-Type", containing("multipart/form-data;"))
+            .withQueryParam("projectKey", equalTo("CALC"))
+            .withQueryParam("testExecKey", equalTo("CALC-2"))
+            .withQueryParam("testPlanKey", equalTo("CALC-3"))
+            .withQueryParam("fixVersion", equalTo("1.0"))
+            .withQueryParam("revision", equalTo("123"))
+            .withQueryParam("testEnvironments", equalTo("chrome"))
+            .withAnyRequestBodyPart(
+                aMultipart()
+                    .withName("file")
+                    .withHeader("Content-Type", containing("application/xml"))
+                    .withBody(equalToXml(report)))
+    );
+    assertThat(result).isSuccessful();
+ }
+
+ @MavenTest
+ @MavenGoal("xray:import-results")
+ @SystemProperty(value = "xray.cloud", content = "false")
+ @SystemProperty(value = "xray.jiraBaseUrl", content = "http://127.0.0.1:"+PORT_NUMBER)
  @SystemProperty(value = "xray.jiraUsername", content = "username")
  @SystemProperty(value = "xray.jiraPassword", content = "password")
  @SystemProperty(value = "xray.reportFormat", content = "junit")
